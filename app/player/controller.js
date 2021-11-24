869be1a3be14e07e4db12fe449160048c40ff4bc
oneight2 <syarif.cibatu@gmail.com>
2021-11-24 20:40:34 +0700

    fix api detail voucher

--- app/player/controller.js
+++ app/player/controller.js
@@ -34,7 +34,12 @@ module.exports = {
       if (!voucher) {
         res.status(404).json({ message: "Voucher Data Not Found" });
       }
-      res.status(200).json({ data: voucher, payment });
+      res.status(200).json({
+        data: {
+          detail: voucher,
+          payment,
+        },
+      });
     } catch (err) {
       res.status(500).json({ message: err.message } || "Internal Serve Erorr ");
     }
